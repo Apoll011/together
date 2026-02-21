f47c65d5899e11ec60cc613383a8794424bde037
apoll011 <tiagorobotik@gmail.com>
2026-02-21 14:45:46 +0000

    MInor fixes in the refactoring PROXY

--- packages/auth/src/globals.d.ts
+++ packages/auth/src/globals.d.ts
@@ -4,4 +4,12 @@ declare module "@clerk/types" {
   type UserPublicMetadata = AppUserMetadata;
 }
 
+declare global {
+  interface CustomJwtSessionClaims {
+    metadata: {
+      onboardingComplete?: boolean
+    }
+  };
+}
+
 export {};
---
+++ packages/auth/src/provider/midleware.ts
@@ -0,0 +1,38 @@
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { NextRequest, NextResponse } from 'next/server'
+
+const isOnboardingRoute = createRouteMatcher(['/join'])
+const isProtectedRoute = createRouteMatcher([])
+const isUnAuthenticated = createRouteMatcher(['/login'])
+
+export default clerkMiddleware(async (auth, req: NextRequest) => {
+  const { isAuthenticated, sessionClaims, redirectToSignIn } = await auth()
+
+  if (isAuthenticated && isUnAuthenticated(req)) {
+    const home = new URL('/', req.url)
+    return NextResponse.redirect(home)
+  }
+
+  if (isAuthenticated && isOnboardingRoute(req)) {
+    return NextResponse.next()
+  }
+
+  if (!isAuthenticated && isProtectedRoute(req)) return redirectToSignIn({ returnBackUrl: req.url })
+
+  if (isAuthenticated && !sessionClaims?.metadata?.onboardingComplete) {
+    const onboardingUrl = new URL('/join', req.url)
+    return NextResponse.redirect(onboardingUrl)
+  }
+
+  if (isAuthenticated && isProtectedRoute(req)) return NextResponse.next()
+
+  if (!isProtectedRoute(req)) return NextResponse.next()
+
+})
+
+export const config = {
+  matcher: [
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/(api|trpc)(.*)',
+  ],
+}
